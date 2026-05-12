cb23908cfe69bf080ed2b99f25bd1852217ac65c
Abhijeetgupta27 <abhijeetg2277@gmail.com>
2026-05-12 18:12:02 +0530

    Implemented ML recommendation engine

--- backend/recommendation-service/recommender.py
+++ backend/recommendation-service/recommender.py
@@ -30,7 +30,110 @@ class CourseRecommender:
 
         print("TF-IDF matrix created successfully")
 
+    def recommend_courses(
+        self,
+        query,
+        difficulty=None,
+        top_n=5
+    ):
+
+        print("\nGenerating recommendations...")
+
+        # ---------------------------------
+        # Convert query into TF-IDF vector
+        # ---------------------------------
+
+        query_vector = self.vectorizer.transform([query.lower()])
+
+        # ---------------------------------
+        # Calculate cosine similarity
+        # ---------------------------------
+
+        similarity_scores = cosine_similarity(
+            query_vector,
+            self.tfidf_matrix
+        ).flatten()
+
+        # ---------------------------------
+        # Copy dataset
+        # ---------------------------------
+
+        results = self.df.copy()
+
+        # ---------------------------------
+        # Add similarity scores
+        # ---------------------------------
+
+        results['similarity'] = similarity_scores
+
+        # ---------------------------------
+        # Filter by difficulty if provided
+        # ---------------------------------
+
+        if difficulty:
+
+            results = results[
+                results['course_difficulty'].str.lower()
+                ==
+                difficulty.lower()
+            ]
+
+        # ---------------------------------
+        # Calculate final weighted score
+        # ---------------------------------
+
+        results['final_score'] = (
+
+            results['similarity']
+
+            *
+
+            results['rating']
+
+            *
+
+            np.log1p(results['enrollment'])
+
+        )
+
+        # ---------------------------------
+        # Sort recommendations
+        # ---------------------------------
+
+        results = results.sort_values(
+            by='final_score',
+            ascending=False
+        )
+
+        # ---------------------------------
+        # Return top recommendations
+        # ---------------------------------
+
+        return results[
+            [
+                'course_title',
+                'course_organization',
+                'course_difficulty',
+                'rating',
+                'enrollment',
+                'similarity',
+                'final_score'
+            ]
+        ].head(top_n)
+    
+
+
 
 if __name__ == "__main__":
 
     recommender = CourseRecommender()
+
+    recommendations = recommender.recommend_courses(
+        query="machine learning",
+        difficulty="Beginner",
+        top_n=5
+    )
+
+    print("\nTop Recommendations:\n")
+
+    print(recommendations)
